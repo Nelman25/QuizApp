b7ef2a2ef75ae09284ebd42714c872fb88f27197
Nelman25 <jonelvillaver735@gmail.com>
2024-08-15 15:40:04 +0800

    questions and choices are now displayed, also added some basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
+import Quiz from "./components/Quiz";
 
 const App = () => {
     return (
         <>
             <Header />
             <main>
-              
+              <Quiz />
             </main>
         </>
     );
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,5 +1,39 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import QUESTIONS from "../Questions.js";
+
 const Quiz = () => {
-    return <div>Currently active question here</div>;
+    const [userAnswers, setUserAnswers] = useState([]);
+    const currentlyActiveQuestion = userAnswers.length;
+
+    function handleSubmitAnswer(selectedAnswer) {
+        setUserAnswers((prevState) => {
+            return [...prevState, selectedAnswer];
+        });
+    }
+
+    return (
+        <div className="w-[50%] shadow-xl h-80% mx-auto my-16 p-16 bg-white rounded-xl">
+            <h1 className="text-2xl text-center m-8 text-slate-600 font-bold">
+                {QUESTIONS[currentlyActiveQuestion].text}
+            </h1>
+            <ul className="w-[90%] my-4 mx-auto">
+                {QUESTIONS[currentlyActiveQuestion].answers.map((answer) => (
+                    <li
+                        key={answer}
+                        className="bg-blue-500 font-bold text-center mb-4 rounded-full shadow-md hover:bg-blue-600 hover:translate-y-[-4px] ease-in duration-300 active:translate-y-0"
+                    >
+                        <button
+                            onClick={() => handleSubmitAnswer(answer)}
+                            className="w-full text-2xl py-4"
+                        >
+                            {answer}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default Quiz;
