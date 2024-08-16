bc1973c7f9b5ac8561f483960467dac15f50c655
Nelman25 <jonelvillaver735@gmail.com>
2024-08-17 06:44:58 +0800

    fixed the problem where the handleSubmitAnswer gets re-created everytime the component re-renders

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,14 +1,16 @@
-/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
 
 const QuestionTimer = ({ timeout, onTimeout }) => {
     const [remainingTime, setRemainingTime] = useState(timeout);
 
     useEffect(() => {
+        console.log("SET TIMEOUT");
         setTimeout(onTimeout, timeout);
     }, [timeout, onTimeout]);
 
     useEffect(() => {
+        console.log("SET INTERVAL");
         setInterval(() => {
             setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
         }, 100);
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,27 +1,30 @@
 /* eslint-disable no-unused-vars */
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import QUESTIONS from "../Questions.js";
-import quizCompleteImg from "../assets/quiz-complete.png";
+import quizCompleted from "../assets/quiz-complete.png";
 import QuestionTimer from "./QuestionTimer.jsx";
 
 const Quiz = () => {
     const [userAnswers, setUserAnswers] = useState([]);
+
     const currentActiveQuestion = userAnswers.length;
     const quizIsComplete = currentActiveQuestion === QUESTIONS.length;
 
-    const handleSubmitAnswer = (selectedAnswer) => {
+    const handleSubmitAnswer = useCallback((selectedAnswer) => {
         setUserAnswers((prevState) => {
             return [...prevState, selectedAnswer];
         });
-    };
+    }, []);
+
+    const handleSkipAnswer = useCallback(() => {
+        handleSubmitAnswer(null);
+    }, [handleSubmitAnswer]);
 
     if (quizIsComplete) {
         return (
-            <div className="bg-white rounded-xl sm:max-w-[700px] mx-auto my-16 p-12 border border-slate-300 shadow-2xl flex flex-col text-center items-center justify-center  ease-in duration-500 translate-y-[-20px]">
-                <h1 className="text-5xl font-bold text-purple-700 mb-8">
-                    Quiz Complete!
-                </h1>
-                <img src={quizCompleteImg} alt="Trophy image" />
+            <div>
+                <h1>Quiz Complete!</h1>
+                <img src={quizCompleted} alt="trophy image" />
             </div>
         );
     }
@@ -30,31 +33,21 @@ const Quiz = () => {
     shuffledAnswer.sort(() => Math.random() - 0.5);
 
     return (
-        <div className="flex justify-center items-center w-2/3 mx-auto">
-            <div className="flex-1 bg-white lg:max-w-[700px] py-16 px-16 sm:min-h-[600px] rounded-xl shadow-2xl border border-slate-500">
-                <QuestionTimer
-                    timeout={10000}
-                    onTimeout={() => handleSubmitAnswer(null)}
-                />
-                <h2 className="text-3xl font-bold leading-normal max-sm:text-lg text-center">
-                    {QUESTIONS[currentActiveQuestion].text}
-                </h2>
-                <ul className="mt-12">
-                    {shuffledAnswer.map((answer) => (
-                        <li
-                            key={answer}
-                            className="bg-blue-500 font-bold text-center mb-4 rounded-full shadow-md hover:text-white hover:bg-purple-600 hover:translate-y-[-4px] ease-in duration-200 active:translate-y-0"
-                        >
-                            <button
-                                onClick={() => handleSubmitAnswer(answer)}
-                                className="w-full text-2xl py-4"
-                            >
-                                {answer}
-                            </button>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+        <div>
+            <QuestionTimer
+                timeout={10000}
+                onTimeout={handleSkipAnswer}
+            />
+            <h2>{QUESTIONS[currentActiveQuestion].text}</h2>
+            <ul>
+                {shuffledAnswer.map((answer) => (
+                    <li key={answer}>
+                        <button onClick={() => handleSubmitAnswer(answer)}>
+                            {answer}
+                        </button>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
