109b3304ff9a0c34f2ce2849dd03e23c56a4ffb0
Nelman25 <jonelvillaver735@gmail.com>
2024-08-16 15:19:30 +0800

    some updates, the choices are now shuffled and i added a pop up when the quiz is completed

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,37 +1,55 @@
 /* eslint-disable no-unused-vars */
 import { useState } from "react";
 import QUESTIONS from "../Questions.js";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 const Quiz = () => {
     const [userAnswers, setUserAnswers] = useState([]);
-    const currentlyActiveQuestion = userAnswers.length;
+    const currentActiveQuestion = userAnswers.length;
+    const quizIsComplete = currentActiveQuestion === QUESTIONS.length;
 
-    function handleSubmitAnswer(selectedAnswer) {
+    const handleSubmitAnswer = (selectedAnswer) => {
         setUserAnswers((prevState) => {
             return [...prevState, selectedAnswer];
         });
+    };
+
+    if (quizIsComplete) {
+        return (
+            <div className="bg-white rounded-xl sm:max-w-[700px] mx-auto my-16 p-12 border border-slate-300 shadow-2xl flex flex-col text-center items-center justify-center  ease-in duration-500 translate-y-[-20px]">
+                <h1 className="text-5xl font-bold text-purple-700 mb-8">
+                    Quiz Complete!
+                </h1>
+                <img src={quizCompleteImg} alt="Trophy image" />
+            </div>
+        );
     }
 
+    const shuffledAnswer = [...QUESTIONS[currentActiveQuestion].answers];
+    shuffledAnswer.sort(() => Math.random() - 0.5);
+
     return (
-        <div className="w-[50%] shadow-xl h-80% mx-auto my-16 p-16 bg-white rounded-xl">
-            <h1 className="text-2xl text-center m-8 text-slate-600 font-bold">
-                {QUESTIONS[currentlyActiveQuestion].text}
-            </h1>
-            <ul className="w-[90%] my-4 mx-auto">
-                {QUESTIONS[currentlyActiveQuestion].answers.map((answer) => (
-                    <li
-                        key={answer}
-                        className="bg-blue-500 font-bold text-center mb-4 rounded-full shadow-md hover:bg-blue-600 hover:translate-y-[-4px] ease-in duration-300 active:translate-y-0"
-                    >
-                        <button
-                            onClick={() => handleSubmitAnswer(answer)}
-                            className="w-full text-2xl py-4"
+        <div className="flex justify-center items-center w-2/3 mx-auto">
+            <div className="flex-1 bg-white lg:max-w-[700px] py-16 px-16 sm:min-h-[600px] rounded-xl shadow-2xl border border-slate-500">
+                <h2 className="text-3xl font-bold leading-normal max-sm:text-lg text-center">
+                    {QUESTIONS[currentActiveQuestion].text}
+                </h2>
+                <ul className="mt-12">
+                    {shuffledAnswer.map((answer) => (
+                        <li
+                            key={answer}
+                            className="bg-blue-500 font-bold text-center mb-4 rounded-full shadow-md hover:text-white hover:bg-purple-600 hover:translate-y-[-4px] ease-in duration-200 active:translate-y-0"
                         >
-                            {answer}
-                        </button>
-                    </li>
-                ))}
-            </ul>
+                            <button
+                                onClick={() => handleSubmitAnswer(answer)}
+                                className="w-full text-2xl py-4"
+                            >
+                                {answer}
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 };
